b8ccdc56a139eb649d944b6e64bd4619247fcc8b
gitsgood <christoforoskhouri@gmail.com>
2026-09-01 14:48:00 +0200

    User can now choose between json or txt output

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,48 @@
 use std::{fs::File, io::Write, ops::Add, path::PathBuf, vec};
 use native_dialog::{DialogBuilder};
-use las::{Reader};
+use las::{Point, Reader};
+use serde::{Serialize};
 
-pub struct LazTextInfo {
-    pub point_count: usize,
-    pub points: Vec<las::Point>
+#[derive(Serialize)]
+pub struct LazPoint {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64
 }
 
-impl LazTextInfo {
-    pub fn new() -> Result<LazTextInfo, Box<dyn std::error::Error>> {
+impl LazPoint {
+    pub fn from(input: Point) -> Result<LazPoint, Box<dyn std::error::Error>> {
+        let parsed_point = LazPoint { x: (input.x), y: (input.y), z: (input.z) };
+        Ok(parsed_point)
+    }
+}
+
+#[derive(Serialize)]
+pub struct LazInfo {
+    pub point_count: usize,
+    pub points: Vec<LazPoint>
+}
+
+impl LazInfo {
+    pub fn new() -> Result<LazInfo, Box<dyn std::error::Error>> {
         let mut reader = Reader::from_path(get_laz_file().unwrap())?;
         let pd = reader.read_all()?;
 
         let mut count: usize = 0;
-        let mut point_vec: Vec<las::Point> = vec![];
+        let mut point_vec: Vec<LazPoint> = vec![];
 
         for wrapped_point in pd.points() {
             count += 1;
             let point = wrapped_point?;
-            point_vec.push(point);
+            let parsed_point = LazPoint::from(point)?;
+            point_vec.push(parsed_point);
         }
 
-        Ok(LazTextInfo { point_count: count, points: point_vec })
+        Ok(LazInfo { point_count: count, points: point_vec })
     }
 
     pub fn print_to_text(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let users_path = get_text_destination().unwrap();
+        let users_path = get_text_destination("Where do you want your LAZ text file placed?").unwrap();
         let output_path = match users_path.into_os_string().into_string() {
             Ok(string) => string.add("/laz_text.txt"),
             Err(os_string) => { 
@@ -40,6 +57,20 @@ impl LazTextInfo {
         for point in &self.points {
             writeln!(file, "{:.2}\t{:.2}\t{:.2}", point.x, point.y, point.z)?;
         }
+
+        Ok(())
+    }
+
+    pub fn print_as_json(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let users_path = get_text_destination("Where do you want your LAZ json file placed?").unwrap();
+        let output_path = match users_path.into_os_string().into_string() {
+            Ok(string) => string.add("/laz_json.json"),
+            Err(os_string) => { 
+                return Err(format!("User's output path contained invalid UTF-8: {:?}", os_string).into());
+            }
+        };
+
+        write_json(&self, &output_path)?;
 
         Ok(())
     }
@@ -58,12 +89,18 @@ pub fn get_laz_file() -> Option<PathBuf> {
     Some(path)
 }
 
-pub fn get_text_destination() -> Option<PathBuf> {
+pub fn get_text_destination(message: &str) -> Option<PathBuf> {
     let path = DialogBuilder::file()
-    .set_title("Where do you want your LAZ text file placed?")
+    .set_title(message)
     .open_single_dir()
     .show()
     .unwrap()?;
 
     Some(path)
+}
+
+pub fn write_json<S: Serialize, P: AsRef<std::path::Path>>(target: &S, path: P) -> Result<(), Box<dyn std::error::Error>> {
+    let file = std::fs::File::create(path.as_ref())?;
+    serde_json::to_writer_pretty(file, target)?;
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
-use laz_to_text::LazTextInfo;
+use laz_to_text::LazInfo;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let laz_info = LazTextInfo::new()?;
+    let laz_info = LazInfo::new()?;
 
-    laz_info.print_to_text()?;
+    println!("What file extension do you wish your data to have?");
+    println!("1: json\nElse: txt");
+
+    let mut users_input = String::new();
+    std::io::stdin().read_line(&mut users_input)?;
+
+    let users_will: u8 = match users_input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("You selected .txt");
+            laz_info.print_to_text()?;
+            return Ok(())
+        }
+    };
+
+    match users_will {
+        1 => {
+            println!("You selected .json");
+            laz_info.print_as_json()?
+        }
+        _ => {
+            println!("You selected .txt");
+            laz_info.print_to_text()?
+        }
+    }
 
     Ok(())
 }
